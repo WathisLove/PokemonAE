d7a72d4a18fef7e0446d33ba394cc6f01bee6236
Redbuzard <mrredbuzard@gmail.com>
2021-05-05 10:32:02 +0200

    Moved all files to a single git directory

---
+++ dev_and_design/tests maps/calis project.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="calis project" tilewidth="16" tileheight="16" margin="6" tilecount="4284" columns="63">
+ <image source="calis project.png" trans="ffffff" width="1022" height="1100"/>
+</tileset>
---
+++ dev_and_design/tilesets/outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="outside" tilewidth="16" tileheight="16" tilecount="7984" columns="8">
+ <image source="../../PokeMania/data/sprites/tilesets/outside_tileset.png" trans="ffffff" width="128" height="15968"/>
+</tileset>
